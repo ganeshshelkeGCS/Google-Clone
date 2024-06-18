94f4e49b55967341a9f7dc8d7367e0d274bdc5cb
ganeshshelkeGCS <91302142+ganeshshelkeGCS@users.noreply.github.com>
2024-06-18 16:33:49 +0530

    added body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
 import HomeHeader from '@/app/components/HomeHeader';
+import Image from 'next/image';
+import GoogleLogo from '@/app/images/google.png';
+import HomeSearch from './components/HomeSearch';
 
 export default function Home() {
   return (
-  <>
-    <HomeHeader />
-  </>
+    <>
+      <HomeHeader />
+      <div className='flex flex-col items-center mt-24'>
+        <Image
+          src={GoogleLogo}
+          //src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png'
+          alt="Google Logo"
+          width={300}
+          height={100}
+          priority
+          style={{width:"auto"}}
+        />
+        <HomeSearch />
+      </div>
+    </>
   );
 }
